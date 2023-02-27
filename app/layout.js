8f8b2febe9a10774a2b621b13acedd96fdc857f5
Amejro <hansonnewton@gmail.com>
2023-02-27 03:24:46 +0000

    update

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import './globals.css'
 import Link from 'next/link'
-import OmniBlog from '../public/omniBlog.svg'
+import OmniBlog from '/public/omniBlog.svg'
 
 export const metadata = {
   title: {
